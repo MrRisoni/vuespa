84d0500a1e717ddabfd441fbcd520d348f04c8db
phoenix <vanguelisk@gmail.com>
2017-08-10 20:51:40 +0300

    port

--- vue_spa.js
+++ vue_spa.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3800;
+const PORT = process.env.PORT || 4800;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, 'dist')));
