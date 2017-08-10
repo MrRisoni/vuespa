7da23c9f195adbb2ef05c5305857229c7ddc957d
phoenix <vanguelisk@gmail.com>
2017-08-10 19:13:50 +0300

    insurance

--- src/components/Passenger/Insurance.vue
+++ src/components/Passenger/Insurance.vue
@@ -6,8 +6,60 @@
             <div class="col-md-12">
 
                 <div class="panel panel-primary">
-                    <div class="panel-heading">insurance</div>
+                    <div class="panel-heading">Insurance</div>
                     <div class="panel-body">
+
+                        <div class="row">
+
+                            <div class="col-md-3">
+
+
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">Insurance</div>
+                                    <div class="panel-body">
+                                        None
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+                            <div class="col-md-3">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">Insurance</div>
+                                    <div class="panel-body">
+                                        None
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+                            <div class="col-md-3">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">Insurance</div>
+                                    <div class="panel-body">
+                                        None
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">Insurance</div>
+                                    <div class="panel-body">
+                                        None
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
 
                     </div>
                 </div>
