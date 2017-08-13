ef78196167b29565a610cda468883f1674e23a99
phoenix <vanguelisk@gmail.com>
2017-08-13 09:20:36 +0300

    i18n

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import Vuex from 'vuex'
 import {sync} from 'vuex-router-sync'
+import VueI18n from 'vue-i18n'
 
 
 require('../node_modules/bootstrap/less/bootstrap.less')
@@ -17,6 +18,25 @@ import store from './store'
 
 sync(store, router)
 
+const translations = {
+    en: {
+        message: {
+            hello: 'hello world'
+        }
+    },
+    ja: {
+        message: {
+            hello: 'こんにちは、世界'
+        }
+    }
+}
+Vue.use(VueI18n)
+
+
+ const i18n = new VueI18n({
+     locale: 'ja', // set locale
+     translations // set locale messages
+ })
 
 /* eslint-disable no-new */
 new Vue({
@@ -24,8 +44,7 @@ new Vue({
     router,
     store,
     template: '<App/>',
-    components: {App},
-
+    components: {App}
 })
 
 
