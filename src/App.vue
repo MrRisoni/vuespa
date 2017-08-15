30abce54db2e1ec8dc8b5b7a48a767e2f40b0aba
phoenix <vanguelisk@gmail.com>
2017-08-15 11:54:59 +0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
 
 
-
     <div id="app">
         <div class="container-fluid">
-            <img class="vueLogo" src="./assets/logo.png">
+
+
+            <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#"> <img class="vueLogo" src="./assets/logo.png"> </a>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+
+
             <router-view></router-view>
         </div>
     </div>
@@ -16,7 +40,13 @@
 
 
     export default {
-        name: 'app'
+        name: 'app',
+        methods: {
+            clickAddLuggage(passengerid) {
+                console.log('Bag Carrier Compo ' + this.passengerid);
+                this.$store.commit('addBaggage', this.passengerid);
+            }
+        }
     }
 </script>
 
@@ -24,7 +54,7 @@
 
 
     .vueLogo {
-        width: 3%;
+        width: 15%;
         height: auto;
     }
 
--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
+        currency: 'EUR',
         paxTypes: [
             {
                 type : 'ADT',
