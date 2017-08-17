2ace0b08ed3f46483bcdc2c43d923dac65c039dc
phoenix <vanguelisk@gmail.com>
2017-08-17 12:03:57 +0300

    hide fare upgrade

--- src/components/Passenger/UpgradeFare/AirlineServices.vue
+++ src/components/Passenger/UpgradeFare/AirlineServices.vue
@@ -1,23 +1,37 @@
 <template>
 
-
     <div class="airlineservices">
 
         <div class="row">
             <div class="col-md-12">
 
-
                 <div class="card">
-                    <div class="card-header bg-info ">{{airline}}</div>
+                    <div class="card-header bg-info ">
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                {{airline}}
+                            </div>
+
+
+                            <div class="col-md-5"></div> <!-- offset does not work -->
+                            
+                            <div class="col-md-2">
+                                <button class="btn btn-sm btn-dark btn-block" @click="toggleMe"> {{toggleButton}}
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </div>
 
                     <div class="card-body">
-
 
                         <div v-for="opt of options">
                             <div class="row">
 
                                 <fare-class :FareClassName="opt.name"
-                                :packages="opt.packages"></fare-class>
+                                            :packages="opt.packages"></fare-class>
 
                             </div>
 
@@ -54,7 +68,21 @@
             'FareClass': FareClass
         },
         data() {
-            return {}
+            return {
+                toggleButton: 'Hide',
+                showMe: true
+            }
+        },
+        methods: {
+            toggleMe() {
+                this.showMe = !this.showMe;
+                if (this.showMe) {
+                    this.toggleButton = 'Hide';
+                }
+                else {
+                    this.toggleButton = 'Show';
+                }
+            }
         }
     }
 </script>
--- src/components/Passenger/UpgradeFare/FareClass.vue
+++ src/components/Passenger/UpgradeFare/FareClass.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="col-md-12">
 
-    <div class="FareClass">
-
+        <div class="FareClass">
 
 
             <div class="card">
-                <div class="card-header">{{FareClassName}}</div>
+                <div class="card-header">
+
+                    <div class="row">
+
+                        <div class="col-md-5">
+                            {{FareClassName}}
+                        </div>
+
+                        <div class="col-md-5"></div> <!-- offset does not work -->
+
+                        <div class="col-md-2">
+                            <button class="btn btn-sm btn-dark btn-block" @click="toggleMe"> {{toggleButton}} </button>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
 
                 <div class="card-body">
 
 
-                    <div v-for="pkg of packages">
+                    <div v-if="showMe">
+                        <div v-for="pkg of packages">
 
 
                         <div v-if="pkg.status === 'free'">
@@ -37,8 +55,7 @@
                         </div>
 
                     </div>
-
-
+                    </div>
 
 
                 </div>
@@ -64,8 +81,22 @@
         props: ['FareClassName', 'packages'],
         data() {
             return {
+                toggleButton: 'Hide',
+                showMe: true
+            }
+        },
+        methods: {
+            toggleMe() {
+                this.showMe = !this.showMe;
+                if (this.showMe) {
+                    this.toggleButton = 'Hide';
+                }
+                else{
+                    this.toggleButton = 'Show';
+                }
             }
         }
+
     }
 </script>
 
